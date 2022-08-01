a84d13ddb25f88a25221cb40e0d56fafa6972db1
lucasrocha0402 <lukasrocha0402@gmail.com>
2022-08-01 12:54:47 -0300

    final

--- src/main/java/br/com/dio/desafio/dominion/Curso.java
+++ src/main/java/br/com/dio/desafio/dominion/Curso.java
@@ -1,47 +1,34 @@
 package br.com.dio.desafio.dominion;
 
-import java.lang.reflect.Constructor;
+public class Curso extends Conteudo {
 
-public class Curso {
 
-    private String tituloDoCurso;
-    private String descricaoDoCurso;
-    private int cargaHorariaDoCurso;
+    private int cargaHoraria;
 
     public Curso() {
 
     }
 
-    public String getTitulo() {
-        return tituloDoCurso;
-    }
 
-    public void setTitulo(String titulo) {
-        this.tituloDoCurso = titulo;
-    }
-
-    public String getDescricao() {
-        return descricaoDoCurso;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricaoDoCurso = descricao;
-    }
-
-    public int getCargaHoraria() {
-        return cargaHorariaDoCurso;
+    public int getCargaHorar() {
+        return cargaHoraria;
     }
 
     public void setCargaHoraria(int cargaHoraria) {
-        this.cargaHorariaDoCurso = cargaHoraria;
+        this.cargaHoraria = cargaHoraria;
     }
 
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + tituloDoCurso + '\'' +
-                ", descricao='" + descricaoDoCurso + '\'' +
-                ", cargaHoraria=" + cargaHorariaDoCurso +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 }
--- src/main/java/br/com/dio/desafio/dominion/Principal.java
+++ src/main/java/br/com/dio/desafio/dominion/Principal.java
@@ -15,13 +15,49 @@ public class Principal {
         curso2.setDescricao("descrição curso c++");
         curso2.setCargaHoraria(6);
 
+
+
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria em java");
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+         */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Lucas:" + devLucas.getConteudoInscritos());
+        devLucas.progredir();
+        devLucas.progredir();
+        devLucas.progredir();
+        System.out.println("--");
+        System.out.println("Conteudos Inscritos Lucas:" + devLucas.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Lucas:" + devLucas.getConteudosConcluidos());
+        System.out.println("XP:" + devLucas.calcularTotalXp());
+
+        System.out.println("---------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos Joao:" + devJoao.getConteudoInscritos());
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Incritos Joao:" + devJoao.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Joao" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
     }
+
 }
